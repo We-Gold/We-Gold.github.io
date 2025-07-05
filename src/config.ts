f519db7980cb4e2c9c5f76a82120c689c29a6ee9
Weaver Goldman <we.goldm@gmail.com>
2025-07-05 19:27:33 -0400

    Fix image display in safari

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Weaver Goldman';
-export const SITE_DESCRIPTION = 'An upcoming freshman at WPI, specializing in CS, ML, and Robotics! See some of my projects, or connect with me.';
+export const SITE_TITLE = "Weaver Goldman"
+export const SITE_DESCRIPTION = `Data Science Master's Student @ WPI, specializing in ML and Software Engineering! See some of my projects or connect with me.`
+
